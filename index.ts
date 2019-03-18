9a65a51dc281cf09f9562fcd8cc32c3f7c41ed66
Anand Chowdhary <mail@anandchowdhary.com>
2019-03-18 17:24:23 +0100

    :sparkles: Add support for translating objects

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import { config } from "dotenv";
 import { Translate } from "@google-cloud/translate";
 import { readJson, writeJson } from "fs-extra";
+import { dot, object } from "dot-object";
+import { languages } from "./languages";
 
 config();
 const googleTranslate = new Translate({
@@ -9,20 +11,42 @@ const googleTranslate = new Translate({
 });
 
 const translate = async (term: string, lang: string) => {
+  if (!languages.includes(lang)) throw new Error(`invalid language: ${lang}`);
   const translation = await googleTranslate.translate(term, lang);
   if (translation.length) return translation[0];
   throw new Error("unable to translate term");
 };
 
+const translateObject = async (originalObject: any, lang: string) => {
+  const dotted = dot(originalObject);
+  for (let key of Object.keys(dotted)) {
+    dotted[key] = await translate(dotted[key], lang);
+  }
+  object(dotted);
+  return dotted;
+};
+
 const translateFile = async (
   fileUrl: string,
   lang: string,
+  from: string = "en",
   write: boolean = false
 ) => {
+  if (!languages.includes(lang)) throw new Error(`invalid language: ${lang}`);
   const contents = await readJson(fileUrl);
+  let isSingleLanguageFile = true;
+  Object.keys(contents).forEach(key => {
+    if (!languages.includes(key)) isSingleLanguageFile = false;
+  });
+  if (isSingleLanguageFile) {
+    // Add new key to the translated file
+  } else {
+    // Generate a lang.json file
+  }
   contents[lang] = { example: true };
   if (write) await writeJson(fileUrl, contents);
+  // console.log("Returning", JSON.stringify(contents));
   return contents;
 };
 
-export { translate, translateFile };
+export { translate, translateObject, translateFile };
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,13 +1,44 @@
-import { translate, translateFile } from "../index";
+import { translate, translateObject, translateFile } from "../index";
 import { join } from "path";
 
 test("translate a word", async () => {
-    const translated = await translate("hello", "fr");
-    expect(translated.toLowerCase().trim()).toBe("bonjour");
+  const translated = await translate("hello", "fr");
+  expect(translated.toLowerCase().trim()).toBe("bonjour");
+});
+
+test("invalid language would fail", () => {
+  translate("hello", "xx")
+    .then(() => {
+      throw new Error("did not fail");
+    })
+    .catch(error => expect(error.message).toBe("invalid language: xx"));
+});
+
+test("translating an object", async () => {
+  interface i {
+    [index: string]: string;
+  }
+  const translated = <i>await translateObject({ hello: "hello" }, "fr");
+  expect(translated.hello.toLowerCase().trim()).toBe("bonjour");
+});
+
+test("translating a more complicated object", async () => {
+  interface i {
+    [index: string]: any;
+  }
+  const translated = <i>await translateObject(
+    {
+      hello: {
+        world: ["hello"]
+      }
+    },
+    "fr"
+  );
+  expect(translated.hello.world[0].toLowerCase().trim()).toBe("bonjour");
 });
 
 test("translating a file", async () => {
-    const filePath = join(__dirname, "example.json");
-    const translated = await translateFile(filePath, "fr");
-    expect(translated.fr).toBeDefined();
+  const filePath = join(__dirname, "example.json");
+  const translated = await translateFile(filePath, "fr");
+  expect(translated.fr).toBeDefined();
 });
