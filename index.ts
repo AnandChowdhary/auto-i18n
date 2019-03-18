325ad27b4ca97ed46e6267e20bb7e1f55e44e0cf
Anand Chowdhary <anandchowdhary@gmail.com>
2019-03-18 14:25:00 +0100

    :zap: Add support for Google Cloud Translation

--- index.ts
+++ index.ts
@@ -1,5 +1,16 @@
+import { config } from "dotenv";
+config();
+
+import { Translate } from "@google-cloud/translate";
+const googleTranslate = new Translate({
+  projectId: process.env.PROJECT_ID,
+  key: process.env.API_KEY
+});
+
 const translate = async (term: string, lang: string) => {
-  return "bonjour";
+  const translation = await googleTranslate.translate(term, lang);
+  if (translation.length) return translation[0];
+  throw new Error("unable to translate term");
 };
 
 export { translate };
